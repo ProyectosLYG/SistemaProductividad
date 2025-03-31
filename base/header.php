cb8e910d7978651d7a2a5ba1c110c1245a88a5dc
Oswaldo Galicia <o.galicia.0812@gmail.com>
2025-03-31 13:18:26 -0600

    separacion de estilos

--- base/header.php
+++ base/header.php
@@ -8,6 +8,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300..700&family=Raleway:ital,wght@0,100..900;1,100..900&family=Roboto+Condensed:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="src/css/styles.css">
+    <link rel="stylesheet" href="src/css/nav-footer.css">
     <title>Desempeño ISC</title>
 </head>
 <body>
