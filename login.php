b0d517049a9072fe30ff5dfeac7784d4eb5dd40e
Oswaldo Galicia <o.galicia.0812@gmail.com>
2025-07-09 12:09:25 -0600

    ajustes a login

--- login.php
+++ login.php
@@ -9,12 +9,12 @@
         <img src="build/img/escudo_isc.png" alt="Descripción" class="img-fluid" style="max-width: 200px;">
         </div>
 
-            <h3 class="text-center mb-5 letra1 letra44">Iniciar Sesión</h3>
+            <h3 class="text-center mb-5 letra1 text-white fw-bold">Iniciar Sesión</h3>
 
 
             <div>
             <fieldset class='mb-5 fildset-border'>
-            <legend class="legend-border mb-2 letra44">Usuario</legend>
+            <legend class="legend-border mb-2 text-white fw-bold">Usuario</legend>
                  <input type="text" class="form-control letra2 input-borde input-bg" id="usuario" name="usuario">
 
             </fieldset>
@@ -23,7 +23,7 @@
             <div class="mb-4"></div>
             <div>
             <fieldset class='mb-3 fildset-border'>
-            <legend class="legend-border mb-2 letra44">Contraseña</legend>
+            <legend class="legend-border mb-2 text-white fw-bold">Contraseña</legend>
                  <input type="password" class="form-control letra2 input-borde input-bg" id="contra" name="contra"required>
 
             </fieldset>
@@ -31,17 +31,17 @@
 
             <div class="form-check mb-5 d-flex justify-content-between">
             
-            <div class="check-letra"> 
-                <input class="form-check-input form-check-input" type="checkbox" id="recordar" name="recordar">
-                <label class="form-check-label letra3" for="recordar"> Recordar contraseña</label>
+            <div class=" d-flex gap-2 align-items-center justify-content-center"> 
+                <input class="form-check-input form-check-input mt-0" type="checkbox" id="recordar" name="recordar">
+                <label class="form-check-label letra3 text-white fw-bold" for="recordar"> Recordar contraseña</label>
             </div>
             
-            <a href="#">¿Olvidaste tu contraseña?</a>
+            <a class="text-white fw-bold" href="#">¿Olvidaste tu contraseña?</a>
         </div>
 
 
             <div class="d-grid">
-                <button type="submit" class="btn btn-dark letra2">Entrar</button>
+                <button type="submit" class="btn btn-dark letra2 py-4">Entrar</button>
             </div>
         </form>
     </div>
