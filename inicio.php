c6867ad0e4bfdafbed03d9de86ca22b99cca8e6c
Lillian Osorio <lillianguadalupeosorio@gmail.com>
2025-07-10 13:41:59 -0600

    ajustes landing

--- inicio.php
+++ inicio.php
@@ -1,8 +1,71 @@
 <?php include "base/header.php" 
 
 ?>
+<section class="hero d-flex align-items-center justify-content-center text-center text-white">
+  <div class="container">
+    <h1 class="display-4 fw-bold">¡Bienvenido a  Desempeño ISC!</h1>
+    <p class="lead lead_hero"> Herramienta de registro y consulta de producción académica.</p>
+    <a href="login.php" class="btn btn-warning mt-3 fs-4">INICIAR
+    </a>
+  </div>
+
+</section>
 <section>
-    
+
+<div id="heroSlider" class="carousel slide">
+  <div class="carousel-inner">
+
+    <!-- Slide 1 -->
+    <div class="carousel-item active">
+      <div class="carousel-image bg-img1">
+        <div class="carousel-caption d-flex flex-column justify-content-center align-items-center">
+          <h1 class="display-4 fw-bold">Bienvenido</h1>
+          <p class="lead lead_hero">Tu plataforma confiable para gestión académica.</p>
+        </div>
+      </div>
+    </div>
+
+    <!-- Slide 2 -->
+    <div class="carousel-item">
+      <div class="carousel-image bg-img2">
+        <div class="carousel-caption d-flex flex-column justify-content-center align-items-center">
+          <h1 class="display-4 fw-bold">Propuetsa de proyectos</h1>
+          <p class="lead lead_hero">Propon tus ideas de proyectos para el área de ISC.</p>
+        </div>
+      </div>
+    </div>
+
+    <!-- Slide 3 -->
+    <div class="carousel-item">
+      <div class="carousel-image bg-img2">
+        <div class="carousel-caption d-flex flex-column justify-content-center align-items-center">
+          <h1 class="display-4 fw-bold">Registro</h1>
+          <p class="lead lead_hero"> de actividades científicas y académicas desde un solo lugar.</p>
+        </div>
+      </div>
+    </div>
+
+    <!-- Slide 4 -->
+    <div class="carousel-item">
+      <div class="carousel-image bg-img2">
+        <div class="carousel-caption d-flex flex-column justify-content-center align-items-center">
+          <h1 class="display-4 fw-bold">DESEMPEÑO ISC</h1>
+          <p class="lead lead_hero">Mejora la producción de investigación.</p>
+        </div>
+      </div>
+    </div>
+
+  </div>
+
+  <!-- Controles manuales -->
+  <button class="carousel-control-prev" type="button" data-bs-target="#heroSlider" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#heroSlider" data-bs-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </button>
+</div>
+
 </section>
 
 <?php include "base/footer.php"
