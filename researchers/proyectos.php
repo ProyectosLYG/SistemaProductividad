70c89c2d977db71b67587361b1ddbbbe1d322fed
Oswaldo Galicia <o.galicia.0812@gmail.com>
2025-07-17 15:30:39 -0600

    cambio de nombre a archivos index -> proyectos | inicio ->index

---
+++ researchers/proyectos.php
@@ -0,0 +1,82 @@
+    <?php include './build/utilities/nav.php'; ?>
+
+    
+    <main class="principal contenedor">
+        <div class="header-proyectos">
+            <h2>Buscar proyectos</h2>
+            <div class="busqueda">
+                <input id="filtro-Proyectos" type="text" placeholder="Buscar...">
+                <a id="buscar-Proyectos" href="#">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="#000" class="bi bi-search" viewBox="0 0 16 16">
+                        <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
+                    </svg>
+                </a>
+                <a id="Filtrar-Proyectos" href="#">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="#000" class="bi bi-funnel" viewBox="0 0 16 16">
+                        <path d="M1.5 1.5A.5.5 0 0 1 2 1h12a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.128.334L10 8.692V13.5a.5.5 0 0 1-.342.474l-3 1A.5.5 0 0 1 6 14.5V8.692L1.628 3.834A.5.5 0 0 1 1.5 3.5zm1 .5v1.308l4.372 4.858A.5.5 0 0 1 7 8.5v5.306l2-.666V8.5a.5.5 0 0 1 .128-.334L13.5 3.308V2z"/>
+                    </svg>
+                </a>
+            </div>
+        </div>
+
+        <div class="proyectos">
+
+            <div class="proyecto">
+                <p class="titulo">Titulo de ejemplo</p>
+                <p class="descripcion">
+                    Praesent finibus tempus eros at placerat. Nam vehicula porta libero, vitae commodo quam. In cursus erat felis, gravida mattis felis pulvinar a. Proin commodo elit ac leo fermentum tincidunt. Nulla porttitor lacus malesuada efficitur malesuada. Sed id ligula at augue rhoncus imperdiet. Sed egestas condimentum vulputate. Aenean ultricies dignissim maximus. Ut vestibulum neque lacus, a laoreet arcu vehicula id. Donec eu fermentum felis. Sed sit amet diam neque. 
+                </p>
+                <p class="fecha">
+                    13/03/23
+                </p>
+                <div class="footerCont">
+                    <p class="nombre-alumno">Galicia Flores Gerardo Oswaldo | ISC</p>
+                    <a href="#" class="boton-claro">Elegir proyecto</a>
+                </div>
+            </div>
+            <div class="proyecto">
+                <p class="titulo">Titulo de ejemplo</p>
+                <p class="descripcion">
+                    Praesent finibus tempus eros at placerat. Nam vehicula porta libero, vitae commodo quam. In cursus erat felis, gravida mattis felis pulvinar a. Proin commodo elit ac leo fermentum tincidunt. Nulla porttitor lacus malesuada efficitur malesuada. Sed id ligula at augue rhoncus imperdiet. Sed egestas condimentum vulputate. Aenean ultricies dignissim maximus. Ut vestibulum neque lacus, a laoreet arcu vehicula id. Donec eu fermentum felis. Sed sit amet diam neque. 
+                </p>
+                <p class="fecha">
+                    13/03/23
+                </p>
+                <div class="footerCont">
+                    <p class="nombre-alumno">Galicia Flores Gerardo Oswaldo | ISC</p>
+                    <a href="#" class="boton-claro">Elegir proyecto</a>
+                </div>
+            </div>
+            <div class="proyecto">
+                <p class="titulo">Titulo de ejemplo</p>
+                <p class="descripcion">
+                    Praesent finibus tempus eros at placerat. Nam vehicula porta libero, vitae commodo quam. In cursus erat felis, gravida mattis felis pulvinar a. Proin commodo elit ac leo fermentum tincidunt. Nulla porttitor lacus malesuada efficitur malesuada. Sed id ligula at augue rhoncus imperdiet. Sed egestas condimentum vulputate. Aenean ultricies dignissim maximus. Ut vestibulum neque lacus, a laoreet arcu vehicula id. Donec eu fermentum felis. Sed sit amet diam neque. 
+                </p>
+                <p class="fecha">
+                    13/03/23
+                </p>
+                <div class="footerCont">
+                    <p class="nombre-alumno">Galicia Flores Gerardo Oswaldo | ISC</p>
+                    <a href="#" class="boton-claro">Elegir proyecto</a>
+                </div>
+            </div>
+            <div class="proyecto">
+                <p class="titulo">Titulo de ejemplo</p>
+                <p class="descripcion">
+                    Praesent finibus tempus eros at placerat. Nam vehicula porta libero, vitae commodo quam. In cursus erat felis, gravida mattis felis pulvinar a. Proin commodo elit ac leo fermentum tincidunt. Nulla porttitor lacus malesuada efficitur malesuada. Sed id ligula at augue rhoncus imperdiet. Sed egestas condimentum vulputate. Aenean ultricies dignissim maximus. Ut vestibulum neque lacus, a laoreet arcu vehicula id. Donec eu fermentum felis. Sed sit amet diam neque. 
+                </p>
+                <p class="fecha">
+                    13/03/23
+                </p>
+                <div class="footerCont">
+                    <p class="nombre-alumno">Galicia Flores Gerardo Oswaldo | ISC</p>
+                    <a href="#" class="boton-claro">Elegir proyecto</a>
+                </div>
+            </div>
+
+        </div>
+        
+        
+    </main>
+
+<?php include './build/utilities/footer.php'; 
