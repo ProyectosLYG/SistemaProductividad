4c7b9a707d6c4c9621ea88429feca02596882591
Oswaldo Galicia <o.galicia.0812@gmail.com>
2025-08-04 16:50:22 -0600

    actualizacion gera 4/8

--- build/js/app.js
+++ build/js/app.js
@@ -1,15 +1,15 @@
 document.addEventListener('DOMContentLoaded', function(){
-    menuHamb();
+    //menuHamb();
     modoFocus();
 });
-function menuHamb (){
-    const nav = document.querySelector('.mobile-menu');
-    nav.addEventListener('click', mostrarNav);
-}
-function mostrarNav(){
-    const nav = document.querySelector('.enlaces');
-    nav.classList.toggle('mostrar');   
-}
+// function menuHamb (){
+//     const nav = document.querySelector('.mobile-menu');
+//     nav.addEventListener('click', mostrarNav);
+// }
+// function mostrarNav(){
+//     const nav = document.querySelector('.enlaces');
+//     nav.classList.toggle('mostrar');   
+// }
 
 function modoFocus(){
     document.querySelector('.focus').focus();
--- build/utilities/footer.php
+++ build/utilities/footer.php
@@ -2,14 +2,14 @@
         <div class="footer-logo">
             <div class="prueba">
 
-                <img src="build/img/escudo_isc.png" alt="escucdo isc">
+                <img src="../build/img/escudo_isc.png" alt="escucdo isc">
             </div>
             <h1 class="noMargin">TESCI || ISC</h1>
         </div>
         <p>Página realizada por alumnos de ISC del TESCI.</p>
     </footer>
 
-    <script src="src/js/app.js"></script>
-    <script src="build/js/bootstrap.min.js"></script>
+    <script src="../build/js/app.js"></script>
+    <script src="../build/js/bootstrap.min.js"></script>
 </body>
 </html>
