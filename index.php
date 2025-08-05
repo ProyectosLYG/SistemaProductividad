8cc3d095263d97756a50c5154f8548552de2e586
Lillian Osorio <lillianguadalupeosorio@gmail.com>
2025-08-05 16:23:40 -0600

    actualizacion de imagenes

--- index.php
+++ index.php
@@ -1,162 +1,159 @@
-<?php include "./build/utilities/nav.php" 
-
-?>
+<?php include "./build/utilities/nav.php" ?>
 
 <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet">
 
-<section class="mt-xxl">
-</section>
+<section class="mt-xxll"></section>
 
 <section>
 
-<div id="heroSlider" class="carousel slide">
-  <div class="carousel-inner">
+  <div id="heroSlider" class="carousel slide">
+    <div class="carousel-inner">
 
-    <!-- Slide 1 -->
-    <div class="carousel-item active">
-      <div class="carousel-image bg-img1">
-        <div class="carousel-caption d-flex flex-column justify-content-center align-items-center">
-          <h1 class="display-4 fw-bold">Bienvenido</h1>
-          <p class="lead lead_hero">Tu plataforma confiable para gestión académica.</p>
+      <!-- Slide 1 -->
+      <div class="carousel-item active">
+        <div class="carousel-image bg-img1">
+          <div class="carousel-caption d-flex flex-column justify-content-center align-items-center">
+           <h1 class="display-4 fw-bold">Bienvenido</h1>
+           <p class="lead lead_hero">Tu plataforma confiable para gestión académica.</p>
+          </div>
         </div>
       </div>
-    </div>
 
-    <!-- Slide 2 -->
-    <div class="carousel-item">
-      <div class="carousel-image bg-img2">
-        <div class="carousel-caption d-flex flex-column justify-content-center align-items-center">
-          <h1 class="display-4 fw-bold">Propuesta de proyectos</h1>
-          <p class="lead lead_hero">Propon tus ideas de proyectos para el área de ISC.</p>
+      <!-- Slide 2 -->
+      <div class="carousel-item">
+        <div class="carousel-image bg-img2">
+          <div class="carousel-caption d-flex flex-column justify-content-center align-items-center">
+            <h1 class="display-4 fw-bold">Propuesta de proyectos</h1>
+            <p class="lead lead_hero">Propón tus ideas de proyectos para el área de ISC.</p>
+          </div>
         </div>
       </div>
-    </div>
 
-    <!-- Slide 3 -->
-    <div class="carousel-item">
-      <div class="carousel-image bg-img3">
-        <div class="carousel-caption d-flex flex-column justify-content-center align-items-center">
-          <h1 class="display-4 fw-bold">Registro</h1>
-          <p class="lead lead_hero"> de actividades científicas y académicas desde un solo lugar.</p>
+     <!-- Slide 3 -->
+      <div class="carousel-item">
+        <div class="carousel-image bg-img3">
+          <div class="carousel-caption d-flex flex-column justify-content-center align-items-center">
+            <h1 class="display-4 fw-bold">Registro</h1>
+            <p class="lead lead_hero"> de actividades científicas y académicas desde un solo lugar.</p>
+          </div>
         </div>
       </div>
-    </div>
 
-    <!-- Slide 4 -->
-    <div class="carousel-item">
-      <div class="carousel-image bg-img4">
-        <div class="carousel-caption d-flex flex-column justify-content-center align-items-center">
-          <h1 class="display-4 fw-bold">DESEMPEÑO ISC</h1>
-          <p class="lead lead_hero">Impulsa tu productividad, fortalece tu impacto.</p>
+      <!-- Slide 4 -->
+      <div class="carousel-item">
+        <div class="carousel-image bg-img4">
+          <div class="carousel-caption d-flex flex-column justify-content-center align-items-center">
+            <h1 class="display-4 fw-bold">DESEMPEÑO ISC</h1>
+            <p class="lead lead_hero">Impulsa tu productividad, fortalece tu impacto.</p>
+          </div>
         </div>
       </div>
-    </div>
+     </div>
 
+    <!-- Controles manuales -->
+    <button class="carousel-control-prev" type="button" data-bs-target="#heroSlider" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon"></span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#heroSlider" data-bs-slide="next">
+     <span class="carousel-control-next-icon"></span>
+   </button>
   </div>
-
-  <!-- Controles manuales -->
-  <button class="carousel-control-prev" type="button" data-bs-target="#heroSlider" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon"></span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#heroSlider" data-bs-slide="next">
-    <span class="carousel-control-next-icon"></span>
-  </button>
-
-</div>
 </section>
 
-<section>
 
-<div class="parent container-fluid text-center mx-auto">
-  <div class="item div1 bg-salmon">
-    <div class="h-100 d-flex flex-column justify-content-center">
+  <!-- CONTADOR -->
+
+<section>
+  <div class="parent container-fluid text-center mx-auto">
+    <div class="item div1 bg-salmon borde-difuminado">
+      <div class="h-100 d-flex flex-column justify-content-center zoom-hover">
         <i class="bi bi-megaphone" style="font-size:100px;" ></i>
         <h3 class="fw-bold mt-2">600</h3>
         <p class="text-uppercase m-0">Artículos Nacionales</p>
       </div>
   </div>
 
-  <div class="item div2 bg-lightblue">
-     <div class="p-4 rounded h-100">
+    <div class="item div2 bg-lightblue borde-difuminado">
+      <div class="h-100 d-flex flex-column justify-content-center zoom-hover">
         <i class="bi bi-cpu" style="font-size:80px;"></i>
         <h3 class="fw-bold mt-2">600</h3>
         <p class="text-uppercase m-0">Competencias Nacionales</p>
       </div>
+    </div>
 
-  </div>
-
-  <div class="item div3 bg-purple">
-    <div class="h-100 d-flex flex-column justify-content-center">
+    <div class="item div3 bg-purple borde-difuminado">
+      <div class="h-100 d-flex flex-column justify-content-center zoom-hover">
         <i class="bi bi-mortarboard" style="font-size:100px;"></i>
         <h3 class="fw-bold mt-2">6000</h3>
         <p class="text-uppercase m-0">Proyectos de Titulación</p>
       </div>
-  </div>
+    </div>
   
-  <div class="item div4 bg-indigo">
-    <div class="p-4 rounded h-100">
+    <div class="item div4 bg-indigo borde-difuminado">
+      <div class="h-100 d-flex flex-column justify-content-center zoom-hover">
         <i class="bi bi-globe-americas" style="font-size:80px;"></i>
         <h3 class="fw-bold mt-2">600</h3>
         <p class="text-uppercase m-0">Artículos Internacionales</p>
       </div>
 
-  </div>
+    </div>
 
-  <div class="item div5 bg-rose">
-    <div class="p-4 rounded h-100">
+    <div class="item div5 bg-rose borde-difuminado">
+     <div class="p-4 rounded h-100 zoom-hover">
         <i class="bi bi-book" style="font-size:80px;"></i>
         <h3 class="fw-bold mt-2">6</h3>
-        <p class="text-uppercase m-0">Libros Publicados</p>
+        <p class="text-uppercase m-0">Libros y Capítulos Publicados</p>
       </div>
+    </div>
   </div>
-
-</div>
 </section>
 
-<section class="container">
-<div class="d-flex flex-row justify-content-center ms-4 me-4 p-4 gap-5">
+  <!-- CONVOCATORIAS -->
+
+<section>
+  <div class="d-flex flex-row justify-content-center ms-4 me-4 p-4 gap-5">
 
     <div class="card" style="width: 38rem;">
-      <img src="build/img/c1.jpeg"class="card-img-top" alt="...">
-      <div class="card-body">
-        <h5 class="card-title">Convocatorias</h5>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-        <a href="#" class="btn btn-warning mt-3 fs-4">Ver más</a>
-      </div>
-    </div>
-
-  <div class="card" style="width: 38rem;">
-      <img src="build/img/c2.jpeg"class="card-img-top" alt="...">
-      <div class="card-body">
-        <h5 class="card-title">Convocatorias</h5>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-        <a href="#" class="btn btn-warning mt-3 fs-4">Ver más</a>
-      </div>
-    </div>
-
-  <div class="card" style="width: 38rem;">
-      <img src="build/img/c3.jpeg"class="card-img-top" alt="...">
-      <div class="card-body">
-        <h5 class="card-title">Convocatorias</h5>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-        <a href="#" class="btn btn-warning mt-3 fs-4">Ver más</a>
-      </div>
+      <img src="build/img/c1.jpeg"class="card-img-top" alt="..."> </img>
+        <div class="card-body">
+          <h5 class="card-title text-center fs-2">Convocatorias</h5>
+          <p class="card-text">Convoca a participar en TecTron TESVG en donde se realizará la competencia de robots en distintas categorías, esta se desarrollará el jueves 3 de julio de 2025 en el tecnológico.</p>
+          <a href="#" class="btn btn-warning mt-3 fs-4 d-flex justify-content-center text-center">Ver más</a>
+        </div>
     </div>
 
     <div class="card" style="width: 38rem;">
-      <img src="build/img/c4.jpeg"class="card-img-top" alt="...">
-      <div class="card-body">
-        <h5 class="card-title">Convocatorias</h5>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-        <a href="#" class="btn btn-warning mt-3 fs-4">Ver más</a>
-      </div>
-  </div>
+      <img src="build/img/c5.jpg"class="card-img-top" alt="..."></img>
+        <div class="card-body">
+          <h5 class="card-title text-center fs-2">Convocatorias</h5>
+          <p class="card-text">El Instituto Tecnológico de Agua Prieta  convoca a docentes, investigadores, estudiantes de posgrado y de licenciatura, nacionales e internacionales.</p>
+          <a href="#" class="btn btn-warning mt-3 fs-4 d-flex justify-content-center text-center">Ver más</a>
+        </div>
+    </div>
 
-</div>
+    <div class="card" style="width: 38rem;">
+      <img src="build/img/c6.jpg"class="card-img-top" alt="..."></img>
+       <div class="card-body">
+          <h5 class="card-title text-center fs-2">Convocatorias</h5>
+          <p class="card-text">El Tecnológico de estudios superiores de Villa Guerrero a través de la carrera de ingeniería electrónica, convoca a participar en TecTron TESVG.</p>
+          <a href="#" class="btn btn-warning mt-3 fs-4 d-flex justify-content-center text-center">Ver más</a>
+       </div>
+    </div>
+
+    <div class="card" style="width: 38rem;">
+      <img src="build/img/c7.jpg"class="card-img-top" alt="..."></img>
+        <div class="card-body">
+          <h5 class="card-title text-center fs-2">Convocatorias</h5>
+          <p class="card-text">Ceremonia de inicio de las estancias del 30 Verano de la Investigación Científica y Tecnológica del Pacífico 2025.  #RumboAl30AniversarioDelfín.</p>
+          <a href="#" class="btn btn-warning mt-3 fs-4 d-flex justify-content-center text-center">Ver más</a>
+      </div>
+    </div>
+
+  </div>
 </section>
 
 
-<section class="container b-4">
+<section class="b-4">
 <div class="d-flex flex-row justify-content-center ms-0 me-0">
 
 <div class="card border-0" style="width: 98rem;">
