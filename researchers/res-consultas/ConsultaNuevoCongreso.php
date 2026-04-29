93db36e6d320f2bc7b6d26ccfcf405e8ed987274
Os Galicia GNOME <o.galicia.0812@gmail.com>
2026-04-29 00:11:16 -0600

    congress  half finished

--- researchers/res-consultas/ConsultaNuevoCongreso.php
+++ researchers/res-consultas/ConsultaNuevoCongreso.php
@@ -1,10 +1,8 @@
 <?php
     session_start();
 
-    include '../../build/config/connection.php';
     $_SESSION['errores'] = "";
     $errores = [];
-    $conn = connect();
 
     $id_res = $_SESSION['user'];
     $fileSizeLimit = 3 * 1024 * 1024;
@@ -15,7 +13,6 @@
     $imageNames = [];
     $imgQuantity = 0;
 
-
     // echo '<pre>';
     // echo var_dump($_FILES);
     // echo '</pre>';
@@ -25,54 +22,6 @@
     // echo'</pre>';
     // exit;
     
-
-    (!isset($_POST['nombreCongreso']) || empty($_POST['nombreCongreso'])) ?
-        $errores[] = "El nombre del congreso es obligatorio." :
-        $nombreCongreso = $_POST['nombreCongreso'];
-    
-    (!isset($_POST['acronimo']) || empty($_POST['acronimo'])) ?
-        $errores[] = "El acrónimo es obligatorio." :
-        $acronimo = $_POST['acronimo'];
-
-    (!isset($_POST['institucion']) || empty($_POST['institucion'])) ?
-        $errores[] = "El nombre de la institución es obligatoria.":
-        $institucion = $_POST['institucion'];
-
-    (!isset($_POST['pais']) || empty($_POST['pais'])) ? 
-        $errores[] = "El pais del conf" :
-        $pais = $_POST['pais'];
-    
-    (!isset($_POST['ciudad']) || empty($_POST['ciudad'])) ?
-        $errores[] = "La ciudad del evento es obligatoria.":
-        $ciudad = $_POST['ciudad'];
-
-    (!isset($_POST['modo']) || empty($_POST['modo'])) ?
-        $errores[] = "Debes ingresar el modo del congreso." :
-        $modo = $_POST['modo'];
-    (!isset($_POST['area']) || empty($_POST['area'])) ?
-        $errores[] = "Debe ingresar el area del tema." :
-        $area = $_POST['area'];
-
-    (!isset($_POST['nivel']) || empty($_POST['nivel'])) ?
-        $errores[] = "Debe ingresar a que nivel fue el congreso." :
-        $nivel = $_POST['nivel'];
-
-    (!isset($_POST['fecha']) || empty($_POST['fecha'])) ?
-        $errores[] = "Debe ingresar la fecha del evento." :
-        $fecha = $_POST['fecha'];
-    
-    (!isset($_POST['rol']) || empty($_POST['rol'])) ?
-        $errores[] = "Es necesario indicar el rol que desarrollo." :
-        $rol = $_POST['rol'];
-    
-    (!isset($_POST['tituloProyecto']) || empty($_POST['tituloProyecto'])) ?
-        $errores[] = "El titulo del proyecto es obligatorio.":
-        $tituloProyecto = $_POST['tituloProyecto'];
-
-    (!isset($_POST['tipo']) || empty($_POST['tipo'])) ?
-        $errores[] = "El tipo del proyecto es obligatorio.":
-        $tipo = $_POST['tipo'];
-
     // imgs validation
     foreach($_FILES['evidencia']['error'] as $i => $error){
         if($error === UPLOAD_ERR_OK &&  !empty($_FILES['evidencia']['name'][$i])){
@@ -101,37 +50,6 @@
         exit;
     }
 
-    $sql = "INSERT INTO congreso (
-    userId,
-    nombreCongreso,
-    acronimo,
-    institucion,
-    pais,
-    ciudad,
-    modo,
-    area,
-    nivel,
-    fecha,
-    rol,
-    tituloProyecto,
-    tipo,
-    evidencia
-    ) VALUES (
-    :id_res,
-    :nombreCongreso,
-    :acronimo,
-    :institucion,
-    :pais,
-    :ciudad,
-    :modo,
-    :area,
-    :nivel,
-    :fecha,
-    :rol,
-    :tituloProyecto,
-    :tipo,
-    :evidencia
-    );";
     try{
         if(!is_dir($folderName)){
             mkdir($folderName);
@@ -142,44 +60,7 @@
             move_uploaded_file($tmp['tmp'], $folderName .'/'.$imageName);
             $evidencia[$i] = $imageName;
         }
-        $stmt = $conn -> prepare($sql);
-        $stmt -> execute([
-            'id_res' => $id_res,
-            'nombreCongreso' => $nombreCongreso,
-            'acronimo' => $acronimo,
-            'institucion' => $institucion,
-            'pais' => $pais,
-            'ciudad' => $ciudad,
-            'modo' => $modo,
-            'area' => $area,
-            'nivel' => $nivel,
-            'fecha' => $fecha,
-            'rol' => $rol,
-            'tituloProyecto' => $tituloProyecto,
-            'tipo' => $tipo,
-            'evidencia' => $evidencia[0]
-            ]);
-        $res = $stmt -> rowCount();
-        if($res > 0){
-            header("Location: ../congresos.php");
-            exit;
-        }
-    }catch(PDOException $error){
+        
+    }catch(Exception $error){
         echo "Error: " . $error -> getMessage();
     }
-
-/*
-    nombreCongreso
-    acronimo
-    institucion
-    pais
-    ciudad
-    modo
-    area
-    nivel
-    fecha
-    rol
-    tituloProyecto
-    tipo
-    ) VALUES (
-*/
